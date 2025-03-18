60b9259368b7e937a136805063b94f8dcfbf3748
Sudo-Abhishek <abhishek.agni2498@gmail.com>
2025-03-19 01:11:05 +0530

    fixes

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -61,7 +61,7 @@ export const ProjectsSection = () => {
             <Card
               key={project.title}
               className="px-8 pt-8 md:pt-12 pb-0 md:px-10 lg:pt-16 lg:px-20 sticky"
-              style={{top : `calc(64px + ${index * 40}px)`}}
+              style={{ top: `calc(64px + ${index * 40}px)` }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
@@ -76,7 +76,10 @@ export const ProjectsSection = () => {
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                     {project.results.map((result) => (
-                      <li className="flex gap-2 text-sm md:text-base  text-white/50 ">
+                      <li
+                        key={result.title}
+                        className="flex gap-2 text-sm md:text-base  text-white/50 "
+                      >
                         <CheckCircleIcon className="size-5 md:size-6" />
                         <span>{result.title}</span>
                       </li>
